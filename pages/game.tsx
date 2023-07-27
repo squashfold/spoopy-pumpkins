6131119f7fd20e03348b70c2db4cc274362cad21
Kara Ebrahim <kebrahim@hubspot.com>
2023-07-27 14:54:51 -0400

    Add separator

--- pages/game.tsx
+++ pages/game.tsx
@@ -30,6 +30,7 @@ export default function Game() {
             <span className={styles.title}>Title</span>
             <button className={styles.ctaButton}>Select</button>
           </div>
+          <span className={styles.separator}>vs</span>
           <div className={styles.card}>
             <img className={styles.image} src="https://source.unsplash.com/gDPaDDy6_WE" alt="apple" width="599" height="420" loading="lazy" />
             <span className={styles.title}>Title</span>
