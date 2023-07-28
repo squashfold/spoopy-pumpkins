8c1a0642947834fad9757f3bd2bd0b4cb18324ce
Charlie <cprincewebdev@gmail.com>
2023-07-28 14:56:31 +0100

    Add images to cards

--- Components/card.tsx
+++ Components/card.tsx
@@ -1,6 +1,7 @@
-// import Image from 'next/image'
 import styles from '@/styles/Card.module.css'
 import type FoodItem from '../interfaces/foodItem';
+import Image from 'next/image'
+
 
 interface CardProps {
   item: FoodItem;
@@ -12,7 +13,7 @@ const Card: React.FC<CardProps> = ({ item, submitAnswer, i }) => {
   return (
     <>
       <div className={styles.card}>
-        <img className={styles.image} src="https://source.unsplash.com/sf_1ZDA1YFw" alt="banana" width="599" height="420" loading="lazy" />
+        <Image className={styles.image} src={`/images/game/${item?.name}.jpg`} alt="" width="599" height="420" loading='eager' />
         <span className={styles.title}>{item.name ? item.name : 'Item name'}</span>
         <button className="ctaButton --small" onClick={(event) => submitAnswer(event, i)}>Select</button>
       </div>
