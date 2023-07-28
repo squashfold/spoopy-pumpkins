6fa0db7637c358a8c7fe4f94fadeabd47fa7bbc8
David Ding <davidftding@gmail.com>
2023-07-28 17:08:01 +0100

    Rework game page layout

--- Components/FoodItemCard.tsx
+++ Components/FoodItemCard.tsx
@@ -14,8 +14,8 @@ const Card: React.FC<CardProps> = ({ item, submitAnswer, i, highlighted }) => {
   return (
     <>
       <button type='button' disabled={highlighted !== '' ? true : false} onClick={(event) => submitAnswer(event, item.value, i)} className={`${styles.card} ${highlighted == 'red' ? styles.red : highlighted == 'green' ? styles.green : ''}`}>
-        <Image className={styles.image} src={`/images/game/${item?.name}.jpg`} alt="" width="599" height="420" loading='eager' />
         <span className={styles.title}>{item.name ? item.name : 'Item name'}</span>
+        <Image className={styles.image} src={`/images/game/${item?.name}.jpg`} alt="" width="600" height="600" loading='eager' />
       </button>
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const IntroPage: React.FC = () => {
   return (
-    <div className={styles.container}>
+    <main>
       <h1 className={styles.title}>Welcome to GameName</h1>
       <p className={styles.text}>
         We&apos;re on a mission to make eco-friendly decisions. Get ready to dive into an exciting game filled with yummy food choices! You&apos;ll be presented with two delicious options and it&apos;s your mission to choose the one with the lower carbon footprint. You&apos;ll have 10 seconds to make your decision. The more correct answers you rack up, the better! So, let&apos;s test your knowledge and see how many green points you can score!
@@ -14,7 +14,7 @@ const IntroPage: React.FC = () => {
           Let&apos;s play
         </Link>
       </div>
-    </div>
+    </main>
   );
 };
 
