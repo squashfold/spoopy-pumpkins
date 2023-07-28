c2993172bc389fecef4eb24cd8918810767229e0
andresllinasr <andresllinasr@gmail.com>
2023-07-28 13:41:54 -0500

    included home button

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -3,11 +3,12 @@ import Link from 'next/link';
 import styles from '../styles/Navbar.module.css';
 
 const Navbar: React.FC = () => {
-  return (
-    <nav className={styles.navbar}>
-      <Link className={styles.leaderboardButton} href="/leaderboard">High Scores</Link>
-    </nav>
-  );
+    return (
+        <nav className={styles.navbar}>
+            <Link className={styles.navButton} href="/">Home</Link>
+            <Link className={styles.navButton} href="/leaderboard">High Scores</Link>
+        </nav>
+    );
 };
 
 export default Navbar;
