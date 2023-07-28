5f4830367dee267b1e9f4f7d54c76f99b75ef3b0
andresllinasr <andresllinasr@gmail.com>
2023-07-28 15:59:46 -0500

    intro page style update

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ const inter = Inter({ subsets: ['latin'] });
 
 const IntroPage: React.FC = () => {
   return (
-    <main>
-      <h1 className={styles.title}>Welcome to GameName</h1>
+    <main className={inter.className} >
+      <h1 className={styles.title}>Welcome to Food Planet Game</h1>
       <p className={styles.text}>
         We&apos;re on a mission to make eco-friendly decisions. Get ready to dive into an exciting game filled with yummy food choices! You&apos;ll be presented with two delicious options and it&apos;s your mission to choose the one with the lower carbon footprint. You&apos;ll have 10 seconds to make your decision. The more correct answers you rack up, the better! So, let&apos;s test your knowledge and see how many green points you can score!
       </p>
@@ -16,7 +16,7 @@ const IntroPage: React.FC = () => {
           Let&apos;s play
         </Link>
       </div>
-    </main>
+    </main >
   );
 };
 
