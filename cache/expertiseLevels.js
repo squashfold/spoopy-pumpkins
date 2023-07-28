a22e419b2c3a8fffbba3313816e72746540cde77
andresllinasr <andresllinasr@gmail.com>
2023-07-28 15:50:17 -0500

    quickfix expertise levels

--- cache/expertiseLevels.js
+++ cache/expertiseLevels.js
@@ -2,7 +2,7 @@ export const expertiseLevels = [
     {
         title: "Earth Steward",
         min: 25,
-        max: 30,
+        max: 999,
         description:
             "You are not one to be easily fooled by food labels. You know that some products may appear harmless to our planet, but you've done your research. You are genuinely concerned about the consequences of climate change and made it a mission to turn almost every aspect of your life into actions that protect our environment. Please keep sharing your choices and inspire others to do the same.",
     },
@@ -15,7 +15,7 @@ export const expertiseLevels = [
     },
     {
         title: "Environmental Newbie",
-        min: 5,
+        min: 0,
         max: 14,
         description:
             "It's totally okay if you're unsure or have doubts about climate change and its consequences. It's natural to wonder if you'll personally witness its impact during your lifetime. But you know what's great? There's always room to learn and grow, and I can help you access more resources to deepen your understanding and take action for the environment. Every small step, even in your food purchases, can make a bigger difference than you might think.",
